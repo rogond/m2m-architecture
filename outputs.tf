aaba9dbb88bebad9af5f623732a565908990b4e4
Rodrigo Gonzalez <rogon@slomins.com>
2025-05-06 10:19:32 -0600

    added api_gateway_url output

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
-// outputs.tf
+output "api_id" {
+  description = "Invoke URL for the API Gateway"
+  value       = module.api_gateway.api_id
+}
+
+output "api_gateway_url" {
+  description = "Invoke URL for the API Gateway"
+  value       = module.api_gateway.invoke_url
+}
