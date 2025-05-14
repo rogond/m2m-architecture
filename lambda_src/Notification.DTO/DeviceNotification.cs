fabe539aab9f80cd58579a015d0355583637a85d
Rodrigo Gonzalez <rogon@slomins.com>
2025-05-14 11:10:17 -0600

    added publish to sns

---
+++ lambda_src/Notification.DTO/DeviceNotification.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.DTO;
+
+public class DeviceNotification
+{
+    public string DeviceId { get; set; } = string.Empty;
+    public string NotificationType { get; set; } = string.Empty;
+    public string NotificationMessage { get; set; } = string.Empty;
+}
---
+++ lambda_src/PublishToSns/FunctionHandler.cs
@@ -0,0 +1,39 @@
+﻿
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.SimpleNotificationService.Model;
+using Amazon.SimpleNotificationService;
+using System.Text.Json;
+using Domain.DTO;
+
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
+namespace PublishToSns;
+
+public class FunctionHandler
+{
+    private readonly IAmazonSimpleNotificationService _snsClient;
+    private readonly string? _topicArn = Environment.GetEnvironmentVariable("TOPIC_ARN");
+
+    public FunctionHandler() => _snsClient = new AmazonSimpleNotificationServiceClient();
+
+    public async Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest request, ILambdaContext context)
+    {
+        var input = JsonSerializer.Deserialize<DeviceNotification>(request.Body!);
+
+        var message = JsonSerializer.Serialize(input);
+
+        var response = await _snsClient.PublishAsync(new PublishRequest
+        {
+            TopicArn = _topicArn,
+            Message = message
+        });
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = JsonSerializer.Serialize(new { MessageId = response.MessageId })
+        };
+    }
+
+}
